f8c6809628bf741f523c5dbfa1dbdc43a40ab761
yash <yashg5304@gmail.com>
2026-02-05 11:03:33 +0530

    initial commit

---
+++ day1/src/App.jsx
@@ -0,0 +1,19 @@
+import Chai from "./chai"
+
+function App() {
+  
+
+  return (
+
+    <>
+    <Chai/>
+    <h1>Radha Vallabh Shri HariVansh</h1>
+    <p>Vrindavan ka kan kan bole shri Radha shri Radha Radha</p>
+    </>
+   
+   
+  );
+
+}
+
+export default App
